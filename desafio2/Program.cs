9403e90395d5ad3b53bb4311903096e5f5f4b0fa
Rhuan <rhuan.limaa@hotmail.com>
2021-01-19 00:22:43 -0300

    Commit inicial

---
+++ desafio2/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace desafio2
+{
+class Desafio {
+    static void Main() {
+        int quilometros = Int32.Parse(Console.ReadLine());
+        int minutos = quilometros * 2;
+        Console.WriteLine($"{minutos} minutos");
+    }
+}
---
+++ desafio3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace desafio3
+{
+    class Desafio
+    {
+    static void Main() {
+        let chico = 300 * 1;
+        let bento = 1500 * 1;
+        let bernardo = 600 * 1;
+        let marina = 1000 * 1;
+        let iara = 150 * 1;
+        let marlene = 225;
+        let total = chico+bento+bernardo+marina+iara+marlene;                       // Digite aqui o calculo total
+        Console.WriteLine(total);
+    }
+}
